c1ecd6c601f48d4c28af72ef4a62eb01bbc3350c
Raghunandan <raghuyamana9@gmail.com>
2025-06-28 14:53:18 +0530

    Modified datautil.js

--- src/utils/datautil.js
+++ src/utils/datautil.js
@@ -1,4 +1,5 @@
 const PATIENT_KEY = 'patients';
+const INCIDENT_KEY = 'incidents';
 
 export const getAllPatients = () => {
     const data = localStorage.getItem(PATIENT_KEY);
@@ -21,4 +22,27 @@ export const updatePatient = (updated) => {
 export const deletePatient = (id) => {
     const patients = getAllPatients().filter((p) => p.id !== id);
     localStorage.setItem(PATIENT_KEY, JSON.stringify(patients));
+}
+
+export const getAllIncidents = () => {
+    const data = localStorage.getItem(INCIDENT_KEY);
+    return data ? JSON.parse(data) : [];
+}
+
+export const addIncident = (incident) => {
+    const incidents = getAllIncidents();
+    incidents.push(incident);
+    localStorage.setItem(INCIDENT_KEY, JSON.stringify(incidents));
+}
+
+export const updateIncident = (updated) => {
+    const incidents = getAllIncidents().map((i) =>
+        i.id === updated.id ? updated:i
+    );
+    localStorage.setItem(INCIDENT_KEY, JSON.stringify(incidents));
+}
+
+export const deleteIncident = (id) => {
+    const incidents = getAllIncidents().filter((i) => i.id !== id);
+    localStorage.setItem(INCIDENT_KEY, JSON.stringify(incidents));
 }
